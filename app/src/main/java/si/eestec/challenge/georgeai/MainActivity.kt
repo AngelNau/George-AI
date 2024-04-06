43b39717669137be41abd7822bc1bca4a0e90ba5
AngelNau <angelnaumov1@gmail.com>
2024-04-06 15:21:15 +0200

    Initial tab layouts.

--- app/src/main/java/si/eestec/challenge/georgeai/MainActivity.kt
+++ app/src/main/java/si/eestec/challenge/georgeai/MainActivity.kt
@@ -5,16 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayoutMediator
+import si.eestec.challenge.georgeai.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityMainBinding
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val NUM_OF_TABS = 3
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+        val toolbar = _binding.toolbar
+        setSupportActionBar(toolbar)
+
+        configureTabLayout()
+    }
+
+    private fun configureTabLayout() {
+        val tabLayout = _binding.tabLayout
+        val viewPager = _binding.viewPager
+        val tabPagerAdapter = TabPagerAdapter(this, NUM_OF_TABS)
+        viewPager.adapter = tabPagerAdapter
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "Tasks"
+//                1 -> tab.text = "Tab 2"
+//                2 -> tab.text = "Tab 3"
+            }
+        }.attach()
     }
 }

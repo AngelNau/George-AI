12e42a13f94a3d430be60eb5c7e7a19cc07d71e1
sivanovska <si3840@student.uni-lj.si>
2024-04-07 08:28:33 +0200

    SharedPreferences

--- app/src/main/java/si/eestec/challenge/georgeai/AddNewTaskFragment.kt
+++ app/src/main/java/si/eestec/challenge/georgeai/AddNewTaskFragment.kt
@@ -2,22 +2,32 @@ package si.eestec.challenge.georgeai
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.DatePicker
-import android.widget.TextView
-import androidx.core.os.bundleOf
+import androidx.core.content.edit
+import androidx.navigation.fragment.findNavController
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import java.util.Calendar
 import si.eestec.challenge.georgeai.databinding.FragmentAddNewTaskBinding
+import si.eestec.challenge.georgeai.model.TaskModel
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class AddNewTaskFragment : Fragment() {
 
 
     private var _binding: FragmentAddNewTaskBinding? = null
+    private lateinit var sharedPreferences: SharedPreferences
+
     private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +40,48 @@ class AddNewTaskFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentAddNewTaskBinding.inflate(inflater, container, false)
+
+        sharedPreferences = requireActivity().getSharedPreferences(TaskFragment.SHARED_PREF, Context.MODE_PRIVATE)
+
+        val submitButton: Button = binding.submitBtn
+        submitButton.setOnClickListener {
+            // Retrieve text from EditText fields
+            val title = binding.titleInputEditText.text.toString()
+            val description = binding.descriptionInputEditText.text.toString()
+            val date = binding.dateInputEditText.text.toString()
+            val time = binding.timeInputEditText.text.toString()
+
+            Log.d("SavedData", "Title: $title, Description: $description, Date: $date, Time: $time")
+
+            // Save data to SharedPreferences
+            val task = TaskModel(title, description, date, time)
+            saveToSharedPref(task)
+
+            // Navigate to TaskFragment
+            findNavController().navigate(R.id.action_add_new_task_fragment_to_task_fragment)
+        }
+
+
         return binding.root
 
+    }
+
+    private fun saveToSharedPref(task: TaskModel) {
+        val tasksString = sharedPreferences.getString("tasks", null)
+        val tasksList: MutableList<TaskModel>
+        if (tasksString.isNullOrBlank()) {
+            tasksList = emptyList<TaskModel>().toMutableList()
+        } else {
+            tasksList = Json.decodeFromString(tasksString)
+        }
+        val timeFormatter = DateTimeFormatter.ofPattern("d/M/yyyy HH:mm")
+        tasksList.add(task)
+        tasksList.sortBy {
+            LocalDate.parse("${it.date} ${it.time}", timeFormatter)
+        }
+        sharedPreferences.edit {
+            putString("tasks", Json.encodeToString(tasksList))
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -43,12 +93,13 @@ class AddNewTaskFragment : Fragment() {
 
     }
 
-    private fun btnShowTimePicker(){
+    private fun btnShowTimePicker() {
         binding.timePickerBtn.setOnClickListener {
             val calendar = Calendar.getInstance()
             val hour = calendar[Calendar.HOUR_OF_DAY]
             val minute = calendar[Calendar.MINUTE]
-            val timePickerDialog = TimePickerDialog(requireContext(),
+            val timePickerDialog = TimePickerDialog(
+                requireContext(),
                 { view, hourOfDay, minute ->
                     val time = String.format("%02d:%02d", hourOfDay, minute)
                     binding.timeInputEditText.setText("$time")
@@ -59,7 +110,7 @@ class AddNewTaskFragment : Fragment() {
         }
     }
 
-    private fun btnShowDatePicker(){
+    private fun btnShowDatePicker() {
         binding.datePicketBtn.setOnClickListener {
             val calendar = Calendar.getInstance()
             val year = calendar.get(Calendar.YEAR)
--- app/src/main/java/si/eestec/challenge/georgeai/TaskFragment.kt
+++ app/src/main/java/si/eestec/challenge/georgeai/TaskFragment.kt
@@ -1,17 +1,33 @@
 package si.eestec.challenge.georgeai
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.tasks.Task
+import kotlinx.serialization.json.Json
 import okhttp3.OkHttpClient
 import si.eestec.challenge.georgeai.databinding.FragmentTaskBinding
+import si.eestec.challenge.georgeai.model.TaskModel
 
 class TaskFragment : Fragment() {
+    companion object {
+        const val SHARED_PREF = "SHARED_PREFERENCES"
+    }
     private lateinit var _binding: FragmentTaskBinding
+    private val binding get() = _binding!!
+
     private lateinit var okHttpClient: OkHttpClient
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerAdapter: RecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,12 +37,38 @@ class TaskFragment : Fragment() {
         _binding = FragmentTaskBinding.inflate(inflater, container, false)
         okHttpClient = OkHttpClient()
         initListeners()
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
         return _binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sharedPreferences = requireContext().getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE)
+        recyclerAdapter = RecyclerAdapter(loadTasks()){}
+        recyclerView.adapter = recyclerAdapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        recyclerAdapter.updateTasks(loadTasks())
     }
 
     private fun initListeners() {
         _binding.newTask.setOnClickListener {
-            findNavController().navigate(TaskFragmentDirections.toCreateTaskFragment())
+            findNavController().navigate(TaskFragmentDirections.toAddNewTaskFragment())
         }
+    }
+
+    private fun loadTasks(): List<TaskModel> {
+        val taskList: MutableList<TaskModel>
+        val taskString = sharedPreferences.getString("tasks", null)
+        if(taskString.isNullOrBlank()) {
+            taskList = emptyList<TaskModel>().toMutableList()
+        } else {
+            taskList = Json.decodeFromString(taskString)
+        }
+
+        return taskList
     }
 }
